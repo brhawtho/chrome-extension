7523ca3537a1a4999637fc4058b14ad41a8b092f
Bryce Hawthorne <hawthornebryce@gmail.com>
2018-02-11 11:49:36 -0800

    fixed popup.js style

--- popup.js
+++ popup.js
@@ -1,50 +1,55 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+/* Copyright (c) 2014 The Chromium Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
 
 /**
  * Get the current URL.
  *
  * @param {function(string)} callback called when the URL of the current tab
- *   is found.
+ *                                    is found.
  */
 function getCurrentTabUrl(callback) {
-    // Query filter to be passed to chrome.tabs.query - see
-    // https://developer.chrome.com/extensions/tabs#method-query
+    /* Query filter to be passed to chrome.tabs.query - see
+     * https://developer.chrome.com/extensions/tabs#method-query
+     */
     var queryInfo = {
         active: true,
         currentWindow: true
     };
 
     chrome.tabs.query(queryInfo, (tabs) => {
-        // chrome.tabs.query invokes the callback with a list of tabs that match the
-        // query. When the popup is opened, there is certainly a window and at least
-        // one tab, so we can safely assume that |tabs| is a non-empty array.
-        // A window can only have one active tab at a time, so the array consists of
-        // exactly one tab.
+        /* chrome.tabs.query invokes the callback with a list of tabs that
+         * match the query. When the popup is opened, there is certainly a
+         * window and at least one tab, so we can safely assume that |tabs| is
+         * a non-empty array. A window can only have one active tab at a time,
+         * so the array consists of exactly one tab. */
         var tab = tabs[0];
 
-        // A tab is a plain object that provides information about the tab.
-        // See https://developer.chrome.com/extensions/tabs#type-Tab
+        /* A tab is a plain object that provides information about the tab.
+         * See https://developer.chrome.com/extensions/tabs#type-Tab
+         */
         var url = tab.url;
 
-        // tab.url is only available if the "activeTab" permission is declared.
-        // If you want to see the URL of other tabs (e.g. after removing active:true
-        // from |queryInfo|), then the "tabs" permission is required to see their
-        // "url" properties.
+        /* tab.url is only available if the "activeTab" permission is declared
+         * If you want to see the URL of other tabs (e.g. after removing
+         * active:true from |queryInfo|), then the "tabs" permission is
+         * required to see their "url" properties.
+         */
         console.assert(typeof url == 'string', 'tab.url should be a string');
 
         callback(url);
     });
 
-    // Most methods of the Chrome extension APIs are asynchronous. This means that
-    // you CANNOT do something like this:
-    //
-    // var url;
-    // chrome.tabs.query(queryInfo, (tabs) => {
-    //   url = tabs[0].url;
-    // });
-    // alert(url); // Shows "undefined", because chrome.tabs.query is async.
+    /* Most methods of the Chrome extension APIs are asynchronous. This means
+     * that you CANNOT do something like this:
+     * 
+     * var url;
+     * chrome.tabs.query(queryInfo, (tabs) => {
+     *     url = tabs[0].url;
+     * });
+     * alert(url); // Shows "undefined", because chrome.tabs.query is async.
+     */
 }
 
 /**
@@ -54,11 +59,12 @@ function getCurrentTabUrl(callback) {
  */
 function changeBackgroundColor(color) {
     var script = 'document.body.style.backgroundColor="' + color + '";';
-    // See https://developer.chrome.com/extensions/tabs#method-executeScript.
-    // chrome.tabs.executeScript allows us to programmatically inject JavaScript
-    // into a page. Since we omit the optional first argument "tabId", the script
-    // is inserted into the active tab of the current window, which serves as the
-    // default.
+    /* See https://developer.chrome.com/extensions/tabs#method-executeScript.
+     * chrome.tabs.executeScript allows us to programmatically inject JavaScript
+     * into a page. Since we omit the optional first argument "tabId", the
+     * script is inserted into the active tab of the current window, which
+     * serves as the default.
+     */
     chrome.tabs.executeScript({
         code: script
     });
@@ -69,12 +75,14 @@ function changeBackgroundColor(color) {
  *
  * @param {string} url URL whose background color is to be retrieved.
  * @param {function(string)} callback called with the saved background color for
- *     the given url on success, or a falsy value if no color is retrieved.
+ *                                    the given url on success, or a falsy
+ *                                    value if no color is retrieved.
  */
 function getSavedBackgroundColor(url, callback) {
-    // See https://developer.chrome.com/apps/storage#type-StorageArea. We check
-    // for chrome.runtime.lastError to ensure correctness even when the API call
-    // fails.
+    /* See https://developer.chrome.com/apps/storage#type-StorageArea. We check
+     * for chrome.runtime.lastError to ensure correctness even when the API call
+     * fails.
+     */
     chrome.storage.sync.get(url, (items) => {
         callback(chrome.runtime.lastError ? null : items[url]);
     });
@@ -89,26 +97,29 @@ function getSavedBackgroundColor(url, callback) {
 function saveBackgroundColor(url, color) {
     var items = {};
     items[url] = color;
-    // See https://developer.chrome.com/apps/storage#type-StorageArea. We omit the
-    // optional callback since we don't need to perform any action once the
-    // background color is saved.
+    /* See https://developer.chrome.com/apps/storage#type-StorageArea. We omit
+     * the optional callback since we don't need to perform any action once the
+     * background color is saved.
+     */
     chrome.storage.sync.set(items);
 }
 
-// This extension loads the saved background color for the current tab if one
-// exists. The user can select a new background color from the dropdown for the
-// current page, and it will be saved as part of the extension's isolated
-// storage. The chrome.storage API is used for this purpose. This is different
-// from the window.localStorage API, which is synchronous and stores data bound
-// to a document's origin. Also, using chrome.storage.sync instead of
-// chrome.storage.local allows the extension data to be synced across multiple
-// user devices.
+/* This extension loads the saved background color for the current tab if one
+ * exists. The user can select a new background color from the dropdown for the
+ * current page, and it will be saved as part of the extension's isolated
+ * storage. The chrome.storage API is used for this purpose. This is different
+ * from the window.localStorage API, which is synchronous and stores data bound
+ * to a document's origin. Also, using chrome.storage.sync instead of
+ * chrome.storage.local allows the extension data to be synced across multiple
+ * user devices.
+ */
 document.addEventListener('DOMContentLoaded', () => {
     getCurrentTabUrl((url) => {
         var dropdown = document.getElementById('dropdown');
 
-        // Load the saved background color for this page and modify the dropdown
-        // value, if needed.
+        /* Load the saved background color for this page and modify the dropdown
+         * value, if needed.
+         */
         getSavedBackgroundColor(url, (savedColor) => {
             if (savedColor) {
                 changeBackgroundColor(savedColor);
@@ -116,8 +127,9 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
 
-        // Ensure the background color is changed and saved when the dropdown
-        // selection changes.
+        /* Ensure the background color is changed and saved when the dropdown
+         * selection changes.
+         */
         dropdown.addEventListener('change', () => {
             changeBackgroundColor(dropdown.value);
             saveBackgroundColor(url, dropdown.value);
